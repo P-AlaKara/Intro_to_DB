d27a3e2a44fa979bd318452da64a36f9c300740d
Priscilla Alakara <140266837+P-AlaKara@users.noreply.github.com>
2025-12-14 21:39:32 +0300

    Refactor create_database function for clarity

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,25 +1,21 @@
 import mysql.connector
-from mysql.connector import Error
 
-def create_database():
-    try:
-        db = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="200303"
-        )
+try:
+    db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="200303"
+    )
 
-        cursor = db.cursor()
-        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-        print("Database 'alx_book_store' created successfully!")
+    cursor = db.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+    print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
-        print(f"Error while connecting to MySQL: {e}")
+except mysql.connector.Error as e:
+    print(f"Error while connecting to MySQL: {e}")
 
-    finally:
-        if 'cursor' in locals():
-            cursor.close()
-        if 'db' in locals() and db.is_connected():
-            db.close()
-
-create_database()
+finally:
+    if 'cursor' in locals():
+        cursor.close()
+    if 'db' in locals() and db.is_connected():
+        db.close()
